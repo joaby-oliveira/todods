118d3ee511d9f009223eab44bb17ba433be8da83
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-11-03 16:53:58 -0300

    Edit login.php page Edit login.php structure.

--- login.php
+++ login.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="public/styles/login.css">
+    <link rel="stylesheet" href="public/styles/global.css">
     <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
@@ -34,26 +32,26 @@
                 <h1>Login</h1>
             </div>
 
-            <div class="email-container">
+            <div class="email-block">
                 <label for="">Email</label>
                 <input type="text" placeholder="email@email.com">
             </div>
 
-            <div class="password-container">
+            <div class="password-block">
                 <label for="">Senha</label>
                 <input type="password" name="" id="" placeholder="********">
             </div>
 
-            <div class="forgot-password-container">
+            <div class="forgotPass-block">
                 <h3>Esqueci minha senha</h3>
             </div>
 
-            <div class="submit-container">
+            <div class="submit-block">
                 <input type="submit" value="Entrar">
             </div>
 
-            <div class="register-container">
-                <a href="">Cadastrar</a>
+            <div class="register-block">
+                <a href="register.php">Cadastrar</a>
             </div>
         </div>
 
