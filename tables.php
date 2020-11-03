840ecff3cea69bf5a430732e44eac808a90dd9b5
VictorNAGomes <victor.gomes63@etec.sp.gov.br>
2020-11-02 21:16:47 -0300

    Modify tables.html to tables.php

---
+++ tables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabelas</title>
+</head>
+<body>
+    <?php include "header-main.php";?>
+    <main class="container">
+        
+        <article class="tables-container">
+            <div class="table-item">
+                <i>...</i>
+                <h3>Tarefas da escola</h3>
+            </div>
+            <div class="table-add">
+                <i>+</i>
+            </div>
+        </article>
+    </main>
+    <?php include "footer.php";?>
+</body>
+</html>
