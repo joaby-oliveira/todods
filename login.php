e16512dd85710992b1a9737fe791602e8dcb0ab6
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-11-03 15:04:20 -0300

    Edit login.php page Add componenents to the page

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/styles/login.css">
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main class="container">
+
+        
+        <div class="flexbox-container-1">
+            <div class="btn-back">
+                <span class="iconify" data-inline="false" data-icon="ion:chevron-back-outline"></span>
+            </div>
+            <div class="main-title">
+                <h1>Fique organizado com o TodoDS</h1>
+            </div>
+        </div>
+
+        <div class="flexbox-container-2">
+            
+            <div class="logo-icon">
+                <img src="public/images/logo.svg" alt="logo">
+            </div>
+
+            <div class="login-title">
+                <h1>Login</h1>
+            </div>
+
+            <div class="email-container">
+                <label for="">Email</label>
+                <input type="text" placeholder="email@email.com">
+            </div>
+
+            <div class="password-container">
+                <label for="">Senha</label>
+                <input type="password" name="" id="" placeholder="********">
+            </div>
+
+            <div class="forgot-password-container">
+                <h3>Esqueci minha senha</h3>
+            </div>
+
+            <div class="submit-container">
+                <input type="submit" value="Entrar">
+            </div>
+
+            <div class="register-container">
+                <a href="">Cadastrar</a>
+            </div>
+        </div>
+
+    </main>
+</body>
+</html>
