59335649a603e208d6b62f74bbe251fec54a19e2
Joaby <joaby.santos@etec.sp.gov.br>
2020-11-03 23:40:38 -0300

    Edit table.php

--- tables.php
+++ tables.php
@@ -15,30 +15,60 @@ include "public/components/Header/header.php";
     <main class="container">
         <?= createHeader("Aqui estão suas tarefas <img src='public/icons/icon-smile.svg'>"); ?>
         <section class="tables-container">
+            
+            <article class="table-item">
+                <a href="#">
+                    <div class="titleOptions-block">
+                        <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
+                        <h3>Tarefas da escola</h3>
+                    </div>
+                </a>
+                <img src="https://images.unsplash.com/photo-1602525963389-fde1e08ca487?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="Imagem de fundo">
+            </article>
 
             <article class="table-item">
-                <div class="titleOptions-block">
-                    <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
-                    <h3>Tarefas da escola</h3>
-                </div>
+                <a href="#">
+                    <div class="titleOptions-block">
+                        <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
+                        <h3>Tarefas da escola</h3>
+                    </div>
+                </a>
                 <img src="https://images.unsplash.com/photo-1602525963389-fde1e08ca487?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="Imagem de fundo">
             </article>
+            
             <article class="table-item">
-                <div class="titleOptions-block">
-                    <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
-                    <h3>Tarefas da escola</h3>
-                </div>
+                <a href="#">
+                    <div class="titleOptions-block">
+                        <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
+                        <h3>Tarefas da escola</h3>
+                    </div>
+                </a>
                 <img src="https://images.unsplash.com/photo-1602525963389-fde1e08ca487?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="Imagem de fundo">
             </article>
+            
             <article class="table-item">
-                <div class="titleOptions-block">
-                    <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
-                    <h3>Tarefas da escola</h3>
-                </div>
+                <a href="#">
+                    <div class="titleOptions-block">
+                        <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
+                        <h3>Tarefas da escola</h3>
+                    </div>
+                </a>
+                <img src="https://images.unsplash.com/photo-1602525963389-fde1e08ca487?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="Imagem de fundo">
+            </article>
+
+            <article class="table-item">
+                <a href="#">
+                    <div class="titleOptions-block">
+                        <i><img src="public/icons/icon-dots.svg" alt="Ver mais"></i>
+                        <h3>Tarefas da escola</h3>
+                    </div>
+                </a>
                 <img src="https://images.unsplash.com/photo-1602525963389-fde1e08ca487?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt="Imagem de fundo">
             </article>
             <article class="table-add table-item">
-                <i><img src="public/icons/icon-plus.svg" alt="Ícone de adicionar tabela "></i>
+                <a href="#">
+                    <i><img src="public/icons/icon-plus.svg" alt="Ícone de adicionar tabela "></i>
+                </a>
             </article>
 
         </section>
