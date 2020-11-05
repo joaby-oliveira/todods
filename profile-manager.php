b45bf6c883380db0e9204f635b3a0171429236a1
VictorNAGomes <victor.gomes63@etec.sp.gv.br>
2020-11-04 23:36:29 -0300

    CREATE profile-manager.php page

---
+++ profile-manager.php
@@ -0,0 +1,88 @@
+<?php
+include "public/components/Header/header.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/styles/profile-manager.css">
+    <link rel="stylesheet" href="public/styles/global.css">
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>   
+    <title>Perfil</title>
+</head>
+<body>
+    <main class="container">
+        <div class="flexbox-container-1">
+            <div class="btn-back">
+                <a href="tables.php"><span class="iconify" data-inline="false" data-icon="ion:chevron-back-outline"></span></a>
+            </div>
+        </div>
+        <div class="flexbox-container-2">
+            <div id="header1">
+                <h2>@jojoTody</h2>
+                <img src="public/icons/icon-dots.svg">
+            </div>
+            <div id="profile-picture">
+                    <img src="https://avatars0.githubusercontent.com/u/55921991?s=400&u=b0d878c174a966fa8719248fa49041d0c3402035&v=4" alt="Foto de perfil">
+                    <p>Online</p>
+            </div>
+            <div id="header2">
+                <div id="desc1">
+                    <p>Just a Task Manager :)</p>
+                </div>
+                <div id="desc2">
+                    <div id="data">
+                        <header>
+                            <strong id="name">Joaby Oliveira dos Santos da Silva</strong>
+                            <strong id="team">Da equipe:</strong>
+                        </header>
+                        <footer>
+                            <p id="followers">5.000 Seguidores</p>
+                            <p id="team-value">Frans & Sis</p>
+                        </footer>
+                    </div>
+                </div>
+                <div class="main-title">
+                    <h1>Informações </h1>
+                    <img src="public/icons/icon-information.svg">
+                </div>
+            </div>
+
+            <div class="flexbox-container-3">
+                
+                <ul>
+                    <div id="personal-data">
+                        <div>
+                            <h2>Dados pessoais</h2>
+                            <a href="#">Editar dados</a>
+                        </div>
+                        <li><strong>Nome</strong><p>Joaby Oliveira dos Santos Silva</p></li>
+                        <li><strong>Nasc.</strong><p>28/04/2002</p></li>
+                        <li><strong>Idade</strong><p>18 Anos</p></li>
+                        <li><strong>Sexo</strong><p>Masculino</p></li>
+                    </div>
+                    <div id="account-data">
+                        <div>
+                            <h2>Dados da conta</h2>
+                        </div>
+                        <li><strong>Email</strong><p>joaby@gmail.com</p></li>
+                        <li><strong>Senha</strong><div id="pass"><p>*******</p><a href="#">alterar senha</a></div></li>
+                        <li><strong>Sexo</strong><p>Masculino</p></li>
+                    </div>
+                    <div id="statistics">
+                        <div>
+                            <h2>Estatísticas</h2>
+                        </div>
+                        <li><strong>Tabelas Criadas</strong><p>100 Tabelas</p></li>
+                        <li><strong>Tarefas Concluídas</strong><p>145 Tabelas</p></li>
+                        <li><strong>Tarefas Inacabadas</strong><p>45 Tabelas</p></li>
+                        <li><strong>Tabelas Favoritadas</strong><p>12 Tabelas</p></li>
+                        <li><strong>Criação da conta</strong><p>15/08/2020</p></li>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
