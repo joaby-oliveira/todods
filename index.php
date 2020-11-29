fe0808c6f0afb054ebe12b429b3d71958fb86f93
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2020-11-29 18:57:17 -0300

    Add index page and css Add style and landing page

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/styles/global.css">
+    <link rel="stylesheet" href="public/styles/index.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    <nav class="index-navbar">
+        <img class="nav-logo" src="public/images/logo.svg">
+        <a class="btn-login" href="login.php">LOGIN</a>
+    </nav>
+   
+</body>
+</html>
